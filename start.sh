426491a4f9d8662c981daedc3ebed8d1a0b1de4a
Zer0 <75836557+zygithub-single@users.noreply.github.com>
2023-03-19 13:52:04 +0800

    Update start.sh
    修改了代理指令添加

--- start.sh
+++ start.sh
@@ -164,6 +164,23 @@ echo -e '\n正在启动Clash服务...'
 Text5="服务启动成功！"
 Text6="服务启动失败！"
 
+cat>/etc/profile.d/clash.sh<<EOF
+# 开启系统代理
+function proxy_on() {
+	export http_proxy=http://127.0.0.1:7890
+	export https_proxy=http://127.0.0.1:7890
+	export no_proxy=127.0.0.1,localhost
+	echo -e "\033[32m[√] 已开启代理\033[0m"
+}
+# 关闭系统代理
+function proxy_off(){
+	unset http_proxy
+	unset https_proxy
+	unset no_proxy
+	echo -e "\033[31m[×] 已关闭代理\033[0m"
+}
+EOF
+
 
 systemctl daemon-reload
 systemctl start clash
